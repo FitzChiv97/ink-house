c2e780466dd16bc1f9f663611c7126dd87e58cce
FitzChiv97 <porter.morgan.97bb@gmail.com>
2026-04-09 10:52:22 +0300

    feat: make products section tabs switch active

--- js/main.js
+++ js/main.js
@@ -1,37 +1,52 @@
 'use strict';
 
 const burgerButton = document.querySelector('.js-burger-button');
+const tabsContainer = document.querySelector('.js-products-tabs');
+const tabsButtons = tabsContainer.querySelectorAll('.js-products-button');
+const productsGrid = document.querySelector('.js-products-grid');
+
+const initialProducts = products.filter(product => product.country === 'Франция');
 
 burgerButton.addEventListener('click', toggleBurgerMenu);
+tabsContainer.addEventListener('click', switchCountry);
+
+renderProducts(initialProducts); // initial call to avoid empty products section
 
 function toggleBurgerMenu() {
     burgerButton.closest('.menu').classList.toggle('_active');
     document.body.classList.toggle('_lock');
-}  
-
-function renderProducts(products, selectedCountry) {
-    let target = document.querySelector('.js-products-grid');
-    target.innerHTML = '';
-
-    for(let picture of products) {
-        if(picture.country === selectedCountry) {
-            target.innerHTML += renderCard(picture);
-        }
-    }
 }
 
-renderProducts(products, 'Германия');
+function switchCountry(e) {
+    if(!e.target.classList.contains('js-products-button')) return; // handle clicks on empty space
+ 
+    tabsButtons.forEach(btn => btn.classList.remove('_active'));
+    e.target.classList.add('_active'); 
 
-function renderCard(picture) {
+    const country = e.target.dataset.country; // render products by selected country
+    const filteredProducts = products.filter(product => product.country === country);
+
+    renderProducts(filteredProducts);
+}
+
+function renderProducts(products) {
+    productsGrid.innerHTML = '';
+
+    products.forEach(product => {
+        productsGrid.innerHTML += renderCard(product);
+    })
+}
+
+function renderCard(product) {
     let card = `
         <div class="products__card card">
             <div class="card__image-container">
-                <img src="${picture.image}" alt="${picture.title} - ${picture.author}" class="card__image">
+                <img src="${product.image}" alt="${product.title} - ${product.author}" class="card__image">
             </div>
-            <p class="card__author">${picture.author}</p>
-            <h3 class="card__name">${picture.title}</h3>
-            <p class="card__features">${picture.features}</p>
-            <span class="card__price">${picture.price} руб</span>
+            <p class="card__author">${product.author}</p>
+            <h3 class="card__name">${product.title}</h3>
+            <p class="card__features">${product.features}</p>
+            <span class="card__price">${product.price} руб</span>
             <button type="button" class="card__button btn">В корзину</button>
         </div>`;
 
