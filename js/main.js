993a9900e659614ac0d8d4f1916d8d8f0fae1c89
FitzChiv97 <porter.morgan.97bb@gmail.com>
2026-04-09 08:54:55 +0300

    feat: add full products dataset in data.js

--- js/main.js
+++ js/main.js
@@ -7,64 +7,7 @@ burgerButton.addEventListener('click', toggleBurgerMenu);
 function toggleBurgerMenu() {
     burgerButton.closest('.menu').classList.toggle('_active');
     document.body.classList.toggle('_lock');
-}
-
-const products = [
-    {
-        id: 1,
-        country: 'Франция',
-        author: 'Марсель Руссо',
-        title: 'Охота Амура',
-        features: 'Холст, масло (50х80)',
-        price: '14 500',
-        image: 'img/products/france/amurs-hunt.jpg',
-    },
-    {
-        id: 2,
-        country: 'Франция',
-        author: 'Анри Селин',
-        title: 'Дама с собачкой',
-        features: 'Акрил, бумага (50х80)',
-        price: '16 500',
-        image: 'img/products/france/lady-with-the-dog.jpg',
-    },
-    {
-        id: 3,
-        country: 'Франция',
-        author: 'Франсуа Дюпон',
-        title: 'Процедура',
-        features: 'Цветная литография (40х60)',
-        price: '20 000',
-        image: 'img/products/france/procedure.jpg',
-    },
-    {
-        id: 4,
-        country: 'Франция',
-        author: 'Луи Детуш',
-        title: 'Роза',
-        features: 'Бумага, акрил (50х80)',
-        price: '12 000',
-        image: 'img/products/france/rose.jpg',
-    },
-    {
-        id: 5,
-        country: 'Франция',
-        author: 'Франсуа Дюпон',
-        title: 'Птичья трапеза',
-        features: 'Цветная литография (40х60)',
-        price: '22 500',
-        image: 'img/products/france/birds-meal.jpg',
-    },
-    {
-        id: 6,
-        country: 'Франция',
-        author: 'Пьер Моранж',
-        title: 'Пейзаж с рыбой',
-        features: 'Цветная литография (40х60) ',
-        price: '20 000',
-        image: 'img/products/france/landscape-with-fish.jpg',
-    }
-];  
+}  
 
 function renderProducts(products, selectedCountry) {
     let target = document.querySelector('.js-products-grid');
@@ -77,7 +20,7 @@ function renderProducts(products, selectedCountry) {
     }
 }
 
-renderProducts(products, 'Франция');
+renderProducts(products, 'Германия');
 
 function renderCard(picture) {
     let card = `
